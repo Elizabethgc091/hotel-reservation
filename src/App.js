4b511985dab5634dbedc69d27244b46d0fda13ba
Elizabeth Gonzalez Cruz <elizabeth.gc091@gmail.com>
2021-11-17 21:38:16 -0600

    add  filterPrice component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import "./App.css";
+import React, { useState } from "react";
 import HotelCard from "./components/HotelCard/HotelCard.js";
 import { hotelsData } from "./data.js";
 import "./components/HotelCard/hotelCard.css";
 import PriceRange from "./components/PriceRange/PriceRange";
 import Header from "./components/Header/Header.js";
+import Select from "./components/Select/Select.js";
 
 function App() {
+  const [price, setPrice] = useState("0");
+  const [filteredHotelsList, setFilteredHotelsList] = useState(hotelsData);
+  function selectFiltered(selectedValue, listUpdated) {
+    setPrice(selectedValue);
+    setFilteredHotelsList(listUpdated);
+    console.log(listUpdated);
+  }
   return (
     <div>
       <div className="App">
         <Header />
+        <Select
+          price={price}
+          selectFilter={selectFiltered}
+          selectFilter={selectFiltered}
+        />
         <div className="hotels-contaniner">
-          {hotelsData.map((hotel, index) => {
+          {filteredHotelsList.map((hotel, index) => {
             return (
               <HotelCard
                 photo={hotel.photo}
